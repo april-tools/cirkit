2865327929c912fb2a2700751cbf8b9bfb69bfa5
lkct <liangkct@yahoo.com>
2023-05-31 17:05:32 +0100

    add credit statement of adapted code

---
+++ cirkit/region_graph/__init__.py
@@ -0,0 +1,7 @@
+# The implementations in
+#     rg_node, region_graph, random_binary_tree, poon_domingos_structure
+# are adapted from
+#     https://github.com/cambridge-mlg/EinsumNetworks/blob/master/src/EinsumNetwork/Graph.py
+# with extensive modifications.
+
+# TODO: should this be here or separately? should we use comment or docstring?
