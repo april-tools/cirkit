e61c5b99a0c259075e3ba7334139c1233d3dc705
lkct <liangkct@yahoo.com>
2023-06-05 19:12:32 +0100

    fix test on einet creation and add more to test

--- tests/test_einet.py
+++ tests/test_einet.py
@@ -1,30 +1,30 @@
-from cirkit.einet.einet import _Args, LowRankEiNet
+from cirkit.einet.einet import LowRankEiNet, _Args
+from cirkit.einet.einsum_layer.cp_einsum_layer import CPEinsumLayer
 from cirkit.einet.exp_family import CategoricalArray
 from cirkit.region_graph.quad_tree import QuadTree
-from cirkit.einet.einsum_layer.cp_einsum_layer import CPEinsumLayer
+from cirkit.region_graph.random_binary_tree import RandomBinaryTree
 
 
-def test_einet_creation():
+def test_einet_creation() -> None:
     """Tests the creation of an einet."""
     device = "cpu"
 
-    # graph = Graph.random_binary_trees(num_var=4, depth=3, num_repetitions=2)
-    graph = QuadTree(4, 4, struct_decomp=True)
+    for graph in (
+        RandomBinaryTree(num_vars=16, depth=3, num_repetitions=2),
+        QuadTree(4, 4, struct_decomp=True),
+    ):
+        args = _Args(
+            rg_structure="quad_tree_stdec",
+            layer_type=CPEinsumLayer,
+            exponential_family=CategoricalArray,
+            exponential_family_args={"k": 2},  # type: ignore[misc]
+            num_sums=16,
+            num_input=16,
+            num_var=16,
+            prod_exp=True,
+            r=1,
+        )
 
-    args = _Args(
-        rg_structure="quad_tree_stdec",
-        layer_type=CPEinsumLayer,
-        exponential_family=CategoricalArray,
-        exponential_family_args={'K': 2},
-        num_sums=16,
-        num_input=16,
-        num_var=16,
-        prod_exp=True,
-        r=1
-    )
-
-    einet = LowRankEiNet(graph, args)
-    einet.initialize(exp_reparam=False, mixing_softmax=False)
-    einet.to(device)
-
-    assert True
+        einet = LowRankEiNet(graph, args)
+        einet.initialize(exp_reparam=False, mixing_softmax=False)
+        einet.to(device)
