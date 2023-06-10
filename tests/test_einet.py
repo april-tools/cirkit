9a2918e9d6f895744e7ba783f51cb20d0b3c39a3
lkct <liangkct@yahoo.com>
2023-06-10 16:52:43 +0100

    test einet for reproducible result

--- tests/test_einet.py
+++ tests/test_einet.py
@@ -12,24 +12,28 @@ from cirkit.region_graph import RegionGraph
 from cirkit.region_graph.poon_domingos_structure import PoonDomingosStructure
 from cirkit.region_graph.quad_tree import QuadTree
 from cirkit.region_graph.random_binary_tree import RandomBinaryTree
+from cirkit.utils import RandomCtx
 
 
 @pytest.mark.parametrize(
-    "rg_cls,kwargs",
+    "rg_cls,kwargs,log_answer",
     [
-        (PoonDomingosStructure, {"shape": [4, 4], "delta": 2}),
-        (QuadTree, {"width": 4, "height": 4, "struct_decomp": False}),
-        (RandomBinaryTree, {"num_vars": 16, "depth": 3, "num_repetitions": 2}),
+        (PoonDomingosStructure, {"shape": [4, 4], "delta": 2}, 10.188161849975586),
+        (QuadTree, {"width": 4, "height": 4, "struct_decomp": False}, 51.31766128540039),
+        (RandomBinaryTree, {"num_vars": 16, "depth": 3, "num_repetitions": 2}, 24.198360443115234),
     ],
 )
+@RandomCtx(42)
 def test_einet_partition_function(
-    rg_cls: Type[RegionGraph], kwargs: Dict[str, Union[int, bool, List[int]]]
+    rg_cls: Type[RegionGraph], kwargs: Dict[str, Union[int, bool, List[int]]], log_answer: float
 ) -> None:
     """Tests the creation and partition of an einet.
 
     Args:
         rg_cls (Type[RegionGraph]): The class of RG to test.
         kwargs (Dict[str, Union[int, bool, List[int]]]): The args for class to test.
+        log_answer (float): The answer of partition func. NOTE: we don't know if it's correct, but \
+            it guarantees reproducibility.
     """
     # TODO: type of kwargs should be refined
     device = "cpu"
@@ -64,4 +68,5 @@ def test_einet_partition_function(
     total_prob = probs.sum(0)
     log_prob = torch.log(total_prob) + out_max
 
+    assert torch.isclose(log_prob, torch.tensor(log_answer), rtol=1e-6, atol=0)
     assert torch.isclose(einet.partition_function(), log_prob, rtol=1e-6, atol=0)
