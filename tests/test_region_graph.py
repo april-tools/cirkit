4b355a5e1cc875bfd9f37e0c960f0ccb0a050bb1
lkct <liangkct@yahoo.com>
2023-06-14 22:00:13 +0100

    add test on RG properties and adjust coverage setting
    also format pyproject.toml

---
+++ tests/test_region_graph.py
@@ -0,0 +1,80 @@
+# pylint: disable=missing-function-docstring
+# TODO: disable checking for docstrings for every test file in tests/
+
+
+from cirkit.region_graph import PartitionNode, RegionGraph, RegionNode
+
+
+def test_smoothness() -> None:
+    rg = RegionGraph()
+    rg.add_node(RegionNode((1, 2)))
+    assert rg.is_smooth
+
+    rg = RegionGraph()
+    rg.add_node(PartitionNode((1, 2)))
+    assert rg.is_smooth
+
+    rg = RegionGraph()
+    rg.add_edge(PartitionNode((1, 2)), RegionNode((1, 2)))
+    assert rg.is_smooth
+
+    rg = RegionGraph()
+    rg.add_edge(PartitionNode((1, 2)), RegionNode((1, 2, 3)))
+    assert not rg.is_smooth
+    assert not rg.is_structured_decomposable
+
+
+def test_decomposability() -> None:
+    rg = RegionGraph()
+    rg.add_node(RegionNode((1, 2)))
+    assert rg.is_decomposable
+
+    rg = RegionGraph()
+    rg.add_node(PartitionNode((1, 2)))
+    assert not rg.is_decomposable
+
+    rg = RegionGraph()
+    rg.add_edge(RegionNode((1, 2)), PartitionNode((1, 2)))
+    assert rg.is_decomposable
+
+    rg = RegionGraph()
+    part = PartitionNode((1, 2))
+    rg.add_edge(RegionNode((1,)), part)
+    rg.add_edge(RegionNode((2,)), part)
+    assert rg.is_decomposable
+
+    rg = RegionGraph()
+    part = PartitionNode((1, 2))
+    rg.add_edge(RegionNode((1,)), part)
+    rg.add_edge(RegionNode((1, 2)), part)
+    assert not rg.is_decomposable
+    assert not rg.is_structured_decomposable
+
+
+def test_structured_decomposablity() -> None:
+    rg = RegionGraph()
+    rg.add_node(RegionNode((1, 2)))
+    assert rg.is_structured_decomposable
+
+    rg = RegionGraph()
+    rg.add_node(PartitionNode((1, 2)))
+    assert not rg.is_structured_decomposable
+
+    rg = RegionGraph()
+    part = PartitionNode((1, 2, 3))
+    rg.add_edge(RegionNode((1, 2)), part)
+    rg.add_edge(RegionNode((3,)), part)
+    rg.add_edge(part, RegionNode((1, 2, 3)))
+    assert rg.is_structured_decomposable
+
+    rg = RegionGraph()
+    region = RegionNode((1, 2, 3))
+    part = PartitionNode((1, 2, 3))
+    rg.add_edge(RegionNode((1, 2)), part)
+    rg.add_edge(RegionNode((3,)), part)
+    rg.add_edge(part, region)
+    part = PartitionNode((1, 2, 3))
+    rg.add_edge(RegionNode((1,)), part)
+    rg.add_edge(RegionNode((2, 3)), part)
+    rg.add_edge(part, region)
+    assert not rg.is_structured_decomposable
