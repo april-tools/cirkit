958d4c79439dc2e2b7ee66f2ef3dc128daa30337
anton <grasarade@gmail.com>
2023-07-13 12:01:45 +0200

    added imports in module init

--- cirkit/layers/sum_product/__init__.py
+++ cirkit/layers/sum_product/__init__.py
@@ -1,2 +1,5 @@
 from .base import SumProductLayer as SumProductLayer
 from .cp import CPLayer as CPLayer
+from .cp_collapsed import CPCollapsedLayer as CPCollapsedLayer
+from .cp_shared import CPSharedLayer as CPSharedLayer
+from .tucker import TuckerLayer as TuckerLayer
