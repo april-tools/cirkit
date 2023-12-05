a2c0555c4ba29d5f2fb4a0ad36684dc77d2e1a6c
lkct <liangkct@yahoo.com>
2023-12-05 19:36:01 +0000

    fix symb circuit/layers with new RG

--- cirkit/new/symbolic/__init__.py
+++ cirkit/new/symbolic/__init__.py
@@ -1,9 +1,5 @@
-# type: ignore
-# pylint: skip-file
-from .symbolic_circuit import SymbolicCircuit
-from .symbolic_layer import (
-    SymbolicInputLayer,
-    SymbolicLayer,
-    SymbolicProductLayer,
-    SymbolicSumLayer,
-)
+from .symbolic_circuit import SymbolicCircuit as SymbolicCircuit
+from .symbolic_layer import SymbolicInputLayer as SymbolicInputLayer
+from .symbolic_layer import SymbolicLayer as SymbolicLayer
+from .symbolic_layer import SymbolicProductLayer as SymbolicProductLayer
+from .symbolic_layer import SymbolicSumLayer as SymbolicSumLayer
--- tests/new/symbolic/test_symbolic_circuit.py
+++ tests/new/symbolic/test_symbolic_circuit.py
@@ -1,48 +1,41 @@
-# type: ignore
-# pylint: skip-file
-
-import pytest
-
+# pylint: disable=missing-function-docstring
 from cirkit.layers.input.exp_family import CategoricalLayer
 from cirkit.layers.sum_product import BaseCPLayer
+from cirkit.new.region_graph import QuadTree, RegionGraph, RegionNode
 from cirkit.new.symbolic.symbolic_circuit import SymbolicCircuit
-from cirkit.new.symbolic.symbolic_layer import (
-    SymbolicInputLayer,
-    SymbolicProductLayer,
-    SymbolicSumLayer,
-)
-from cirkit.region_graph import PartitionNode, RegionGraph, RegionNode
-from cirkit.region_graph.quad_tree import QuadTree
+from cirkit.new.symbolic.symbolic_layer import SymbolicInputLayer, SymbolicSumLayer
 from cirkit.reparams.leaf import ReparamExp
 
-efamily_cls = CategoricalLayer
-efamily_kwargs = {"num_categories": 256}
-layer_cls = BaseCPLayer
-layer_kwargs = {"rank": 1}
-reparam = ReparamExp
 
-num_units = 3
+def test_symbolic_circuit() -> None:
+    efamily_cls = CategoricalLayer
+    efamily_kwargs = {"num_categories": 256}
+    layer_cls = BaseCPLayer
+    layer_kwargs = {"rank": 1}
+    reparam = ReparamExp
 
-
-def test_symbolic_circuit():
     rg = RegionGraph()
     node1 = RegionNode((1,))
     node2 = RegionNode((2,))
-    partition = PartitionNode((1, 2))
     region = RegionNode((1, 2))
-    rg.add_edge(node1, partition)
-    rg.add_edge(node2, partition)
-    rg.add_edge(partition, region)
+    rg.add_partitioning(region, [node1, node2])
 
     circuit = SymbolicCircuit(
         rg, layer_cls, efamily_cls, layer_kwargs, efamily_kwargs, reparam, 4, 4, 1, 1
     )
 
     assert len(list(circuit.layers)) == 4
-    assert any(isinstance(layer, SymbolicInputLayer) for layer in circuit.input_layers)
-    assert any(isinstance(layer, SymbolicSumLayer) for layer in circuit.output_layers)
+    # Ignore: SymbolicInputLayer contains Any.
+    assert all(
+        isinstance(layer, SymbolicInputLayer)  # type: ignore[misc]
+        for layer in circuit.input_layers
+    )
+    # Ignore: SymbolicSumLayer contains Any.
+    assert all(
+        isinstance(layer, SymbolicSumLayer) for layer in circuit.output_layers  # type: ignore[misc]
+    )
 
-    rg_2 = QuadTree(4, 4, struct_decomp=True)
+    rg_2 = QuadTree((4, 4), struct_decomp=True)
 
     circuit_2 = SymbolicCircuit(
         rg_2, layer_cls, efamily_cls, layer_kwargs, efamily_kwargs, reparam, 4, 4, 1, 1
@@ -51,4 +44,4 @@ def test_symbolic_circuit():
     assert len(list(circuit_2.layers)) == 46
     assert len(list(circuit_2.input_layers)) == 16
     assert len(list(circuit_2.output_layers)) == 1
-    assert (circuit_2.scope) == frozenset({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})
+    assert (circuit_2.scope) == frozenset(range(16))
