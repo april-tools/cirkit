8a6718ca693dc8795ccdd0b52a651487c0daa761
Andreas Grivas <andreasgrv@gmail.com>
2024-11-11 12:31:14 +0000

    Add tests for batched integration masks

--- tests/backend/torch/test_queries/test_integration.py
+++ tests/backend/torch/test_queries/test_integration.py
@@ -18,6 +18,8 @@ from tests.symbolic.test_utils import build_monotonic_structured_categorical_cpt
 )
 def test_query_marginalize_monotonic_pc_categorical(semiring: str, fold: bool, optimize: bool):
     compiler = TorchCompiler(semiring=semiring, fold=fold, optimize=optimize)
+    # The following function computes a circuit where we have computed the
+    # partition function and a marginal by hand.
     sc, gt_outputs, gt_partition_func = build_monotonic_structured_categorical_cpt_pc(
         return_ground_truth=True
     )
@@ -44,3 +46,76 @@ def test_query_marginalize_monotonic_pc_categorical(semiring: str, fold: bool, o
     mar_scores2 = mar_query(mar_worlds, integrate_vars=Scope([4]))
     assert mar_scores1.shape == mar_scores2.shape
     assert allclose(mar_scores1, mar_scores2)
+
+
+@pytest.mark.parametrize(
+    "semiring,fold,optimize",
+    itertools.product(["lse-sum", "sum-product"], [False, True], [False, True]),
+)
+def test_batch_query_marginalize_monotonic_pc_categorical(semiring: str, fold: bool, optimize: bool):
+    # Check using a mask with batching works
+    compiler = TorchCompiler(semiring=semiring, fold=fold, optimize=optimize)
+    # The following function computes a circuit where we have computed the
+    # partition function and a marginal by hand.
+    sc, gt_outputs, gt_partition_func = build_monotonic_structured_categorical_cpt_pc(
+        return_ground_truth=True
+    )
+
+    tc: TorchCircuit = compiler.compile(sc)
+
+    # The marginal has been computed for (1, 0, 1, 1, None) -- so marginalising var 4.
+    inputs = torch.tensor([[[1, 0, 1, 1, 1], [1, 0, 1, 1, 1]]], dtype=torch.int64).view(2, 1, 5)
+
+    mar_query = IntegrateQuery(tc)
+    # Create two masks, one is marginalising out everything
+    # and another is marginalising out only the last variable
+    mask = [Scope([0, 1, 2, 3, 4]), Scope([4])]
+    # The first score should be partition function, as we marginalised out all vars.
+    # The second score, should be our precomputed marginal.
+    mar_scores = mar_query(inputs, integrate_vars=mask)
+
+    if semiring == 'sum-product':
+        assert torch.isclose(mar_scores[0], torch.tensor(gt_partition_func))
+        assert torch.isclose(mar_scores[1], torch.tensor(gt_outputs['mar'][(1, 0, 1, 1, None)]))
+    elif semiring == 'lse-sum':
+        mar_scores = torch.exp(mar_scores)
+        assert torch.isclose(mar_scores[0], torch.tensor(gt_partition_func))
+        assert torch.isclose(mar_scores[1], torch.tensor(gt_outputs['mar'][(1, 0, 1, 1, None)]))
+    else:
+        raise ValueError('Unexpected semiring: "%s"' % semiring)
+
+
+@pytest.mark.parametrize(
+    "semiring,fold,optimize",
+    itertools.product(["lse-sum", "sum-product"], [False, True], [False, True]),
+)
+def test_batch_broadcast_query_marginalize_monotonic_pc_categorical(semiring: str, fold: bool, optimize: bool):
+    # Check that passing a single mask results in broadcasting
+    compiler = TorchCompiler(semiring=semiring, fold=fold, optimize=optimize)
+    # The following function computes a circuit where we have computed the
+    # partition function and a marginal by hand.
+    sc, gt_outputs, gt_partition_func = build_monotonic_structured_categorical_cpt_pc(
+        return_ground_truth=True
+    )
+
+    tc: TorchCircuit = compiler.compile(sc)
+
+    # The marginal has been computed for (1, 0, 1, 1, None) -- so marginalising var 4.
+    inputs = torch.tensor([[[1, 0, 1, 1, 0], [1, 0, 1, 1, 1]]], dtype=torch.int64).view(2, 1, 5)
+
+    mar_query = IntegrateQuery(tc)
+    # Create a single mask - this should be broadcast along the batch dim.
+    mask = Scope([4])
+    # The first score should be partition function, as we marginalised out all vars.
+    # The second score, should be our precomputed marginal.
+    mar_scores = mar_query(inputs, integrate_vars=mask)
+
+    if semiring == 'sum-product':
+        assert torch.isclose(mar_scores[0], torch.tensor(gt_outputs['mar'][(1, 0, 1, 1, None)]))
+        assert torch.isclose(mar_scores[1], torch.tensor(gt_outputs['mar'][(1, 0, 1, 1, None)]))
+    elif semiring == 'lse-sum':
+        mar_scores = torch.exp(mar_scores)
+        assert torch.isclose(mar_scores[0], torch.tensor(gt_outputs['mar'][(1, 0, 1, 1, None)]))
+        assert torch.isclose(mar_scores[1], torch.tensor(gt_outputs['mar'][(1, 0, 1, 1, None)]))
+    else:
+        raise ValueError('Unexpected semiring: "%s"' % semiring)
