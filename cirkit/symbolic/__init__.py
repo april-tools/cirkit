58c84087bc97d009239496300ee918a9d02e3d9c
Andreas Grivas <andreasgrv@gmail.com>
2024-12-13 16:05:25 +0000

    Improve docstrings

--- cirkit/symbolic/__init__.py
+++ cirkit/symbolic/__init__.py
@@ -1,3 +1,7 @@
 """
-This module implements circuits at the symbolic level, i.e. it defines operations on circuits, circuit components and the computational graph that specifies how to map inputs to outputs.
+This module implements abstract symbolic representations of circuits.
+These symbolic representations need compiling to some backend before they can be executed or used to perform inference with.
+More specifically, the module defines operations on circuits, circuit components and the computational graph that defines the computed function.
+The parameters are symbolically defined but not materialised/allocated in memory.
+While data types are defined here, they are abstract (int, real, complex); the precision is decided at compile time based on the backend.
 """
